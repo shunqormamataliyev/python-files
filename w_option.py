8512ea4ec01035516a50495d0ada9d1662099ba4
ShunqorMamataliyev <shunqormamataliyev737@gmail.com>
2025-01-22 19:29:20 +0500

    yangi

--- w_option.py
+++ w_option.py
@@ -1,4 +1,8 @@
-#file ga yozish
-with open("example.txt","w") as file:
-    file.write("bu yangi fayl ,azmuni\n")
-    file.write("ikkinchi qator")
+# #file ga yozish
+# with open("example.txt","w") as file:
+#     file.write("bu yangi fayl ,azmuni\n")
+#     file.write("ikkinchi qator")
+
+with open("example.txt","a") as file:
+    file.write("bu yangi qo'shilgan 1-qator\n")
+    file.write("bu yangi qo'shilgan 2-qator")
