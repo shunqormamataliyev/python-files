db2ca503fb77af5060a13248364c36fe7edc8c92
ShunqorMamataliyev <shunqormamataliyev737@gmail.com>
2025-01-22 20:00:55 +0500

    yahbjwdhbc

--- os_module.py
+++ os_module.py
@@ -1,10 +1,21 @@
+# import os
+#
+# #faylni o'chirish
+#
+# file_path = "example.txt"
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+#     print(f"Fole {file_path} o'chirildi")
+# else:
+#     print('Fayl mavjud emas')
+
 import os
 
-#faylni o'chirish
+dir_path = "/manzil"
 
-file_path = "example.txt"
-if os.path.exists(file_path):
-    os.remove(file_path)
-    print(f"Fole {file_path} o'chirildi")
+if os.path.exists((dir_path)) and os.path.isdir(dir_path):
+    os.rmdir(dir_path)  #bo'sh direktoriyani o'chiradi
+    print(f"Direktoriya {dir_path} o'chirildi")
+
 else:
-    print('Fayl mavjud emas')
+    print(("Direktoriyaning mavjud emasligi yoki u bo'sh emasligi"))
